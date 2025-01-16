9c859374b4619da4e3d87d54dd2695124d9dd2f8
MrRobot <olivier.portal@laplateforme.io>
2025-01-16 11:42:07 +0100

    Fix: debug json write and link to display_history

--- functions/normalize_history.py
+++
@@ -1,8 +0,0 @@
-from .normalize_item import normalize_item
-
-def normalize_history(history):
-    item = {"operation_tuple": []}
-    normalize_item(item)
-    
-    return [normalize_item(item) for item in history]
-    
